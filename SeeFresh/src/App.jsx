d98d2888e79f325df4dc7e6a5c542496f4574f25
Siluni Keerthiratne <siluni@keerthiratne.com>
2024-01-22 08:04:07 +0530

    React project

---
+++ SeeFresh/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import CustomWebcam from "./assets/Components/CustomWebcam/CustomWebcam"; // import it
+
+function App() {
+  return (
+    <div className='container'>
+      <h1 className='text-sky-400'>SeeFresh</h1>
+      <CustomWebcam />
+      
+      </div>
+    
+    
+  )
+}
+
+export default App
