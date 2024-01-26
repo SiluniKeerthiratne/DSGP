bc6d8707019e9c0bb2e7fffa4fcce413a3702b39
Siluni Keerthiratne <siluni@keerthiratne.com>
2024-01-26 18:07:15 +0530

    initial set-up of server.py

--- SeeFresh/src/App.jsx
+++ SeeFresh/src/App.jsx
@@ -1,16 +1,27 @@
 import './App.css'
 import CustomWebcam from "./assets/Components/CustomWebcam/CustomWebcam"; // import it
+import React, {useState, useEffect}from 'react';
 
 function App() {
+  const [data, setData] = useState([{}])
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:5000/members").then(
+      res=> res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  },[])
   return (
     <div className='container'>
       <h1 className='text-sky-400'>SeeFresh</h1>
       <CustomWebcam />
-      
       </div>
-    
-    
   )
 }
 
 export default App
+
--- flask-server/server/server.py
+++ flask-server/server/server.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+import pickle
+import numpy as np
+
+model = pickle.load(open('iri.pkl', 'rb'))
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route("/members")
 def members():
-    return {'members': ['m1','m2','m3']}
+    return jsonify({"members": ["m1", "m2", "m3"]})
 
 if __name__ == "__main__":
     app.run(debug=True)
