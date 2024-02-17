106dadef04d5a44aededad7c0e8d1774ae11c91d
Siluni Keerthiratne <siluni@keerthiratne.com>
2024-02-17 08:51:32 +0530

    before connecting yolov8

--- SeeFresh/src/pages/OutputPage/OutputPage.jsx
+++ SeeFresh/src/pages/OutputPage/OutputPage.jsx
@@ -1,7 +1,7 @@
 const OutputPage = ({ prediction }) => {
   return (
-      <div>
-          <h3 className="text-white font-poppins font-semibold text-base">{prediction}</h3>
+      <div className='flex flex-col justify-center font-poppins text-white text-3xl mx-12 text-center font-semibold font-style: normal'>
+          <h3>{prediction}</h3>
       </div>
   );
 };
--- SeeFresh/src/pages/inputPage/CustomWebcam.jsx
+++ SeeFresh/src/pages/inputPage/CustomWebcam.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Webcam from 'react-webcam';
 
 const CustomWebcam = ({ webcamRef }) => {
-    const videoConstraints = {
-        width: 420,
-        height: 620,
-        facingMode: 'user',
-    };
+    // Hook to get window size
+    function useWindowSize() {
+        const [windowSize, setWindowSize] = useState({
+            width: undefined,
+            height: undefined,
+        });
+
+        useEffect(() => {
+            function handleResize() {
+                setWindowSize({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+            }
+            
+            window.addEventListener("resize", handleResize);
+            handleResize();
+            return () => window.removeEventListener("resize", handleResize);
+        }, []);
+        
+        return windowSize;
+    }
+
+    const size = useWindowSize(); // Getting the window size
+
+    // Calculating the aspect ratio
+    const isLandscape = size.height <= size.width;
+    const ratio = isLandscape ? size.width / size.height : size.height / size.width;
 
     return (
         <div className='flex flex-col justify-between w-full mx-6 mt-6'>
@@ -14,9 +37,9 @@ const CustomWebcam = ({ webcamRef }) => {
                 audio={false}
                 ref={webcamRef}
                 screenshotFormat='image/jpeg'
-                height={400}
-                width={400}
-                videoConstraints={videoConstraints}
+                height={size.height} // Setting height dynamically
+                width={size.width} // Setting width dynamically
+                videoConstraints={{ facingMode: 'user', aspectRatio: ratio }}
             />
         </div>
     );
