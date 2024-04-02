d38f8acd7a65438c9bdc650aceff9a6bf5856404
Siluni Keerthiratne <siluni@keerthiratne.com>
2024-04-02 12:24:07 +0530

    updated voice components

--- SeeFresh/src/App.jsx
+++ SeeFresh/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import InputPage from "./pages/inputPage/InputPage";
 import OutputOne from "./pages/OutputOne/OutputOne";
 import Intro from "./pages/IntroPage/IntroPage";
@@ -8,6 +8,31 @@ import './App.css';
 
 const App = () => {
   const [detectedObjectData, setDetectedObjectData] = useState("initial");
+ 
+  // useEffect(() => {
+  //   return () => {
+  //     setDetectedObjectData("initial")
+  //     console.log("initll cajngrf")
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    return () => {
+      if (detectedObjectData === "two") {
+        handlePlay("camera is on");
+        console.log("in use effect");
+      } else if (
+        detectedObjectData.isObjectDetected ) {
+        console.log("here in output 2")
+        const OutPutText = `A ${detectedObjectData.objectClass} is detected on the screen, should we capture it. Tap on the screen if you want to capture it`;
+        handlePlay(OutPutText);
+      }
+    };
+  }, [detectedObjectData]);
+
+  const beep = new Audio('/beep-01a.mp3'); // Replace 'beep.mp3' with your audio file path
+      // beep.play();
+  
 
   // Reset detectedObjectData to null when OutputOne is loaded
   const handleOutputOneLoad = () => {
@@ -24,19 +49,19 @@ const App = () => {
 
   const textInto = "Welcome to SeeFresh. Everything you need to know about your groceries just one click away!. Tap screen to continue"
   const textSetUp = "Tap once on the  the screen, we will indicate with a “beep” if an object is placed within the screen, then tap the the screen again to capture."
+  
 
   const handleClick = () => {
     if(detectedObjectData === "initial"){
       handlePlay(textInto);
       setDetectedObjectData("one")
     } else if (detectedObjectData === "one") {
-      setDetectedObjectData("two-initial");
-    } else if (detectedObjectData === "two-initial") {
+      setDetectedObjectData("two");
       handlePlay(textSetUp);
-      setDetectedObjectData("two")
     } else if (detectedObjectData === "two") {
+      // handlePlay("camera is on");
       setDetectedObjectData(null);
-    }
+    } 
   };
 
   useEffect(() => {
